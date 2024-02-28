3f42a703215d0cd2e2b735f948c4394fcce85955
JaloJasela <jasela@yandex.ru>
2024-02-29 01:15:41 +0300

    Initial Commit

---
+++ IOS12-HW13-Ershova-Olesya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS12-HW13-Ershova-Olesya
+//
+//  Created by JaloJasela on 29.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
